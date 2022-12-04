9737283519d1318320c7860a1f758fdcfdd2fb47
Jonas <jonasah97@gmail.com>
2022-12-04 16:21:23 +0100

    Update IApiSettings.cs

--- pullAD/pullADs/Facade/IApiSettings.cs
+++ pullAD/pullADs/Facade/IApiSettings.cs
@@ -4,6 +4,8 @@ public interface IApiSettings
 {
     string? AdUrl { get; set; }
     string? RedisConn { get; set; }
+    string? RabbitConn { get; set; }
+
     int TimerInterval1 { get; }
     int TimerInterval2 { get; }
 }
