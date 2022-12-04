ce495a11a0aa181da5bece2aed1dedb5d6fd57c7
Jonas <jonasah97@gmail.com>
2022-12-04 16:21:18 +0100

    Create RabbitMQProducer.cs

---
+++ pullAD/pullADs/RabbitMQProducer.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using pullADs.Facade;
+using RabbitMQ.Client;
+using Serilog;
+
+namespace pullADs;
+
+public class RabbitMQProducer : IMessageProducer
+{
+    public void SendMessage<T>(T message)
+    {
+        Log.Information("Started producing");
+        var factory = new ConnectionFactory {HostName = "localhost"};
+        var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: "ads",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var json = JsonConvert.SerializeObject(message);
+        var body = Encoding.UTF8.GetBytes(json);
+        channel.BasicPublish(exchange: "",
+            routingKey: "ads",
+            basicProperties: null,
+            body: body);
+        Log.Information(" [x] Sent {0}", message);
+    }
+}
