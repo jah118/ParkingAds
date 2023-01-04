a381b6fdfb7126a98048e6176d47bf214891f1d1
Jonas <jonasah97@gmail.com>
2023-01-04 22:15:03 +0100

    Setup of base worker

---
+++ PullParkingService/PullParkingService/util/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace PullParkingService.util;
+
+public class AppSettings : IAppSettings
+{
+    public string? RedisConn { get; set; }
+    public string? RabbitConn { get; set; }
+    public string? RabbitQueueNameConsume { get; set; }
+    public string? RabbitQueueNameProduce { get; set; }
+}
+
+public interface IAppSettings
+{
+    public string? RedisConn { get; set; }
+    public string? RabbitConn { get; set; }
+    public string? RabbitQueueNameConsume { get; set; }
+    public string? RabbitQueueNameProduce { get; set; }
+
+}
