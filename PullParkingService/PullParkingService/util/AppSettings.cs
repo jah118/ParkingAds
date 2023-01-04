1405afaa397c6cd3aa06488eaba2eb56a2da9992
Jonas <jonasah97@gmail.com>
2023-01-04 22:55:09 +0100

    Update AppSettings.cs

--- PullParkingService/PullParkingService/util/AppSettings.cs
+++ PullParkingService/PullParkingService/util/AppSettings.cs
@@ -2,17 +2,23 @@
 
 public class AppSettings : IAppSettings
 {
+    public string? URL { get; set; }
     public string? RedisConn { get; set; }
     public string? RabbitConn { get; set; }
-    public string? RabbitQueueNameConsume { get; set; }
     public string? RabbitQueueNameProduce { get; set; }
+
+    public int TimerInterval1 { get; set; } = 60000;
+    public int TimerInterval2 { get; set; } = 120000;
 }
 
 public interface IAppSettings
 {
+    public string? URL { get; set; }
+
     public string? RedisConn { get; set; }
     public string? RabbitConn { get; set; }
-    public string? RabbitQueueNameConsume { get; set; }
     public string? RabbitQueueNameProduce { get; set; }
 
+    public int TimerInterval1 { get; set; }
+    public int TimerInterval2 { get; set; }
 }
