40ac995a85b7c041785ab888736beb3a2ba4a3ad
Jonas <jonasah97@gmail.com>
2023-01-05 10:03:46 +0100

    added new project for redisconsumerParking

--- RedisConsumerPullAds/.idea/.idea.RedisConsumerPullAds/.idea/sonarlint/issuestore/index.pb
+++ RedisConsumerPullAds/.idea/.idea.RedisConsumerPullAds/.idea/sonarlint/issuestore/index.pb
@@ -3,9 +3,3 @@ H
 RedisConsumerPullAds.sln,0\1\01da62382f5daf4cf4de1795c325227176f3281e
 O
 RedisConsumerPullAds/.gitignore,2\2\222461eb752f91d84a9f7a105eff0c751e43cb20
-b
-2RedisConsumerPullAds/Facade/IAppSettingsHandler.cs,f\6\f611f0f2068e88dce2e47b3149204d5d354d16f3
-_
-/RedisConsumerPullAds/util/AppSettingsHandler.cs,7\e\7e4f9c4da6c9f365b69c3f39026ba58c9b0cc5d6
-X
-(RedisConsumerPullAds/appsettingsOld.json,3\5\3519f21397fcfc454881ce8653adc17ed34d64a7
--- RedisConsumerPullAds/RedisConsumerPullAds/util/AppSettings.cs
+++ RedisConsumerPullAds/RedisConsumerPullAds/util/AppSettings.cs
@@ -12,8 +12,10 @@ public class AppSettings : IAppSettings
     public string? UserName { get; set; }
     public string? Password { get; set; }
     
-    public string? QueueName { get; set; }
+    public string? QueueName1 { get; set; }
+    public string? QueueName2 { get; set; }
     public string? RedisConnectionString { get; set; }
-    public string? RedisKey { get; set; }
+    public string? RedisKey1 { get; set; }
+    public string? RedisKey2 { get; set; }
 
 }
