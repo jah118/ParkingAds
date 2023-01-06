28ab8e15612e93b810c29ddd352de1de587de4f3
Jonas <jonasah97@gmail.com>
2023-01-06 12:02:03 +0100

    Added json format as data send and cleanup of old models

---
+++ ParkingAdsAPI/ParkingAdsAPI/Data/JSONModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ParkingAdsAPI.Data;
+
+public class JSONModel
+{
+    
+    public string searchedLocation  { get; set; }
+    public string topicKey { get; set; }
+    public Sesssion sesssion { get; set; }
+    public string adData { get; set; }
+    public ParkingData parkingData { get; set; }
+}
+
+public class Sesssion
+{
+    public string time_sent { get; set; }
+    public string  messageId { get; set; }
+    public string aggregatorTarget { get; set; }
+    public int splitCounter { get; set; }
+}
+
+public class ParkingData
+{
+    public string data { get; set; }
+    public List<string>? dataList { get; set; }
+
+    
+}
